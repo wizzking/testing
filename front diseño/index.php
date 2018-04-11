d18ce8b6ef96ff26829ebb1434b7904fa7edc30f
daniel <2015030282@upsin.edu.mx>
2018-04-11 02:33:01 -0600

    fromt home 80%

---
+++ front diseño/index.php
@@ -0,0 +1,167 @@
+<!-- /////////////////////////////////////////////////////////////////////////////////// -->
+<!-- //////////////////////////////////////////////////////	forma parte del header 		-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<!-- ////////////////////////////////////////////////////// fin del header 				 -->
+<!-- /////////////////////////////////////////////////////////////////////////////////// -->
+<!-- //////////////////////////////////////////////////////contenido home 				-->
+	<link rel="stylesheet" href="css/home.css">
+	<section class="headerIMG">
+		<img src="img/home/fondoHeaderHome.jpg" alt="banner">
+		<div class="col-xs-12 centenHeader">
+			<div class="col-xs-2 text-right BorderR"><strong>WAPIK</strong></div>
+			<div class="col-xs-3">
+				<img src="img/home/cell.png" alt="cell">
+				<span>+62 202 555 0117</span>
+			</div>
+			<div class="col-xs-7 text-right">
+				<ul class="pull-right">
+					<li><a href="#">HOME</a></li>
+					<li><a href="#">ABOUT US</a></li>
+					<li><a href="#">pricing</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<div class="container contenidoH">
+		<section class="row">
+			<div class="conteTextHeader">
+				<h1>Welcome to Wapik</h1>
+				<div class="row">
+					<p class="col-xs-5">Wapik is multipurpose template, with modern and smart design. W</p>
+				</div>
+				<div class="row" style="margin-left:0px;"><button class="btn-success">LEARN MORE</button></div>
+			</div>
+		</section>
+	</div>
+	<div class="container fullCont">
+		<section class="row text-center">
+			<h2 class="titles">Best of our features</h2>
+			<p class="col-xs-6 col-xs-offset-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus illo unde error esse nam quia porro quaerat. Quisquam mollitia adipisci vero est </p>
+		</section>
+
+		<section class="row" style="margin-top: 50px;">
+			<div class="container text-center">
+				<div class="col-xs-12">
+					<div class="col-xs-4">
+						<img src="img/home/ionic.png" alt="icono">
+						<h4 class="titles">Ionicons</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus officia maiores, provident nihil, mollitia laudantium quo illum aut non ipsam blanditiis possimus, obcaecati inventore at, vel fugit dolor adipisci harum.</p>
+					</div>
+					<div class="col-xs-4">
+						<img src="img/home/quimica.png" alt="icono">
+						<h4 class="titles">Creative Design</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ducimus laborum deleniti quidem placeat consectetur consequatur reiciendis maiores quis natus illo, assumenda magni eius omnis similique atque magnam, autem reprehenderit.</p>
+					</div>
+					<div class="col-xs-4">
+						<img src="img/home/cuadro.png" alt="icono">
+						<h4 class="titles">Photoshop and Sketch</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, asperiores earum harum culpa nemo consectetur. Maiores perspiciatis laboriosam odio veritatis, quod optio doloribus iste dolores, cumque consectetur harum, similique repudiandae!</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="row" style="margin-top: 120px;">
+			<div class="container">
+				<div class="col-xs-12">
+					<div class="row">
+						<div class="col-xs-6">
+							<img src="img/home/esfera.png" alt="icono">
+							<strong class="titles">Cross Browser Compatibility</strong>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima voluptate consequatur mollitiaaesentium nulla tenetur? Nostrum, dignissimos, vitae?</p>
+						</div>
+						<div class="col-xs-6">
+							<img src="img/home/idea.png" alt="icono">
+							<strong class="titles">Creative Idea</strong>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam deserunt quod nostrum voluptatee illum laboriosam, ducimus, incidunt. Necessitatibus!</p>
+						</div>
+					</div>
+					<div class="row" style="margin-top:30px;">
+						<div class="col-xs-6">
+							<img src="img/home/pixel.png" alt="icono">
+							<strong class="titles">Pixel Perfect</strong>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex hic delectus praesentium veritatis obcaecavoluptas voluptates totam error!</p>
+						</div>
+						<div class="col-xs-6">
+							<img src="img/home/nuve.png" alt="icono">
+							<strong class="titles">Free Forever and Ever</strong>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum laborum obcaecati iure, facilis eveniet nihil minima qui blanditiis adipisci recusandae.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<section style="margin-top: 120px; background: #eef1ec;">
+			<div class="container section2">
+				<section class="row text-center" style="margin-top:30px;">
+					<h3 class="titles">Looking for the perfect template to use?</h3>
+					<p>Donec id elit non mi porta gravida at eget metus.</p>
+				</section>
+
+				<section class="row">
+					<div class="container">
+						<div class="col-xs-6 text-center">
+							<img src="img/home/mano.jpg" alt="celuar">
+						</div>
+						<div class="col-xs-6">
+							<div style="margin-top:150px;">
+								<h3 class="titles">Responsive Ready</h3>
+								<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna. Vestibulum id ligula porta felis euismod semper.</p>
+								
+								<div class="row">
+									<img src="img/home/check.png" alt="check" style="margin-left:10px;">
+									<span style="color: #83a7a5;">Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</span>
+								</div>
+								<div class="row">
+									<img src="img/home/check.png" alt="check" style="margin-left:10px;">
+									<span style="color: #83a7a5;">Donec id elit non mi porta gravida at eget metus.</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+	</section>
+
+	<section style="margin-top:60px;">
+		<div class="container">
+			<div class="col-xs-6" >
+				<p style="color:#83a7a5; font-size: 25px; margin-top:80px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque vel eos natus provident similique aut error, rerum ipsum sit voluptate quis nisi molestiae itaque, doloremque necessitatibus voluptatum. Excepturi culpa, beatae!</p>
+
+				<p class="ti" style="color: #6fb048; margin-top:80px;">Valeria Boltneva</p>
+				<p class="ti" style="color: #83a7a5;">Director of Acme.Inc</p>
+			</div>
+			<div class="col-xs-6 text-center" >
+				<img class="imgCircular" src="img/home/mujer.jpg" alt="foto">
+			</div>
+		</div>
+	</section>
+
+	<section style="margin-top:60px; background: #eef1ec; height: 450px;">
+		<img src="img/home/mapa.png" alt="mapa" class="mapa">
+		<div class="container">
+			<div class="col-xs-6" style="margin-top:10%;">
+				<h3 class="titles">Built with High Attention to Details</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis iure, quos quaerat labore error non fugit sint. Quis sapiente, ea? Non sit, aliquam eaque repellat veritatis eveniet maxime consequuntur dolorum.lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero praesentium velit earum maxime nemo, cumque. Recusandae dicta quisquam enim, tenetur unde sit, voluptates libero, vero mollitia velit, perferendis magnam odio.</p>
+			</div>
+		</div>
+		
+	</section>
+
+<!-- ////////////////////////////////////////////////////// fin del home 				 -->
+<!-- /////////////////////////////////////////////////////////////////////////////////// -->
+<!-- //////////////////////////////////////////////////////  forma parte del footer		-->
+</body>
+</html>
+<!-- ////////////////////////////////////////////////////// fin del footer 				 -->
+<!-- /////////////////////////////////////////////////////////////////////////////////// -->
